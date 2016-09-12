14f8231c8f7f475f9ebaf635c2824292d701f5fb
pretzelfisch <aaron@microfisch.com>
2016-09-12 10:40:15 -0700

    create project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlipperyFisch
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            var port = System.Configuration.ConfigurationManager.AppSettings["ListenPort"];
+            if ( string.IsNullOrWhiteSpace ( port ) )
+                port = "8888";
+
+            var serveMe = new SimpleHTTPServer("", int.Parse( port));
+            System.Console.WriteLine ( "Now listeing on port: " + port );
+            System.Console.ReadKey ();
+            serveMe.Stop ();
+            
+        }
+    }
+}
